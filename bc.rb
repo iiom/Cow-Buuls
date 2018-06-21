fc09b604d31c753cf4cac86d74a43587f721c049
iiom <ioma766@gmail.com>
2018-06-21 18:34:38 +0300

    params

--- bc.rb
+++ bc.rb
@@ -1,20 +1,22 @@
-a = (0..9).to_a.shuffle.first(4)   #загадывает 4-значное число, состоящее из неповторяющихся цифр
-errors = 0
+uniq_random_number = (0..9).to_a.shuffle.first(4)   #загадывает 4-значное число,
+errors = 0                                    #состоящее из неповторяющихся цифр
 
-while errors < 5                                   #за ограниченное число ходов.
+while errors < 5                                    #за ограниченное число ходов
   p "puts 4 numbers, u have #{5 - errors} trys"
-  b = STDIN.gets.chomp.to_i                                     #ввод из консоли
+  user_input = STDIN.gets.chomp.to_i                            #ввод из консоли
 
-  if a.join.to_i == b                                # Ваша задача - угадать его
+  if uniq_random_number.join.to_i == user_input
     puts "bingo, u win!"
     exit
   else
-    c = []
-    p b = b.to_s.split('').map(&:to_i)      # То есть если загадано число “1234”,
-    b.size.times {|i| c << b[i] if a[i] == b[i]}          #а вы называете “6531”,
-    b -= c                                                # то результатом будет
-    p "#{c.join(',')} buls" if c != []                      #и 1 бык (цифра “3”)
-    p "#{(a & b).join(',')} cow" if (a & b) != []          #1 корова (цифра “1”)
+    array_temp = []
+    user_input = user_input.to_s.split('').map(&:to_i)
+    user_input.size.times {|i| array_temp << user_input[i] if
+    uniq_random_number[i] == user_input[i]}
+    user_input -= array_temp
+    puts "#{array_temp.join(',')} buls" if array_temp != []
+    puts "#{(uniq_random_number & user_input).join(',')} cow" if
+    (uniq_random_number & user_input) != []
   end
     errors += 1
 end
