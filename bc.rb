fe4a1a4658d877190d77d6c411146d6c5cd86a3d
iiom <ioma766@gmail.com>
2018-06-22 16:35:01 +0300

    with class & modules

--- bc.rb
+++ bc.rb
@@ -1,48 +1,80 @@
-puts "Greetins young gamer. I'm glad to to provide to you this wonderful game." +
-"Your task - to guess it for limited number of the courses. Cows (the digit is" +
-"guessed, but its line item - is not present) and bulls act as hints" +
-"(when both the digit and its line item matches). That is if number 1234 is" +
-"thought, and you call 6531, then 1 cow (digit 1) and 1 bull (digit 3)" +
-"will be result."
 
-# ручной ввод уникального числа при вызове файла и передаче его в качестве аргумента
-uniq_random_number = ARGV[0].split('').map(&:to_i)
+class Bulls
 
-# если числе не было переданно, то компъюктер сам его генерирует
-if uniq_random_number == nil
-  uniq_random_number = (0..9).to_a.shuffle.first(4)
-end
-
-# ошибки и количество ходов
-errors = 0
-turn_numbers = 5
-
-# проверка условия на оставшиеся кол-во ходов, и ручной ввод цифр пользователем
-while errors < turn_numbers
-  p "puts 4 numbers, u have #{5 - errors} trys"
-  user_input = STDIN.gets.chomp.to_i
-
-# условие на победку при отгадывании числа, else сравнение элементов двух
-# массивов по индексам, при совпаденнии " buls" добавление его в новый массив.
-# Из массива цифр введённого пользователем удаляем массив получившися при совпадении.
-# При пересечении получившегося массива с массивом загаданным компътером получаем
-# совпадение для "cow"
-  if uniq_random_number.join.to_i == user_input
-    puts "bingo, u win!"
-    exit
-  else
-    array_temp = []
-    user_input = user_input.to_s.split('').map(&:to_i)
-    user_input.size.times do |i|
-      array_temp << user_input[i] if
-      uniq_random_number[i] == user_input[i]
-    end
-    user_input -= array_temp
-    puts "#{array_temp.join(',')} buls" if array_temp != []
-    puts "#{(uniq_random_number & user_input).join(',')} cow" if
-    (uniq_random_number & user_input) != []
+  def initialize
+    @uniq_random_number = nil
+    @user_input = nil
+    @array_bulls = []
+    @array_cow = []
+    @error = 0
+    @turn_numbers = 5
   end
-    errors += 1
+
+  # puts "Greetins young gamer. I'm glad to to provide to you this wonderful game." +
+  # "Your task - to guess it for limited number of the courses. Cows (the digit is" +
+  # "guessed, but its line item - is not present) and bulls act as hints" +
+  # "(when both the digit and its line item matches). That is if number 1234 is" +
+  # "thought, and you call 6531, then 1 cow (digit 1) and 1 bull (digit 3)" +
+  # "will be result."
+
+  def get_unirandnumber
+
+    @uniq_random_number = ARGV[0]
+
+    if @uniq_random_number != nil
+        @uniq_random_number = @uniq_random_number.split('').map(&:to_i)
+      else
+        @uniq_random_number = (0..9).to_a.shuffle.first(4)
+    end
+  end
+
+  def run
+    while @error < @turn_numbers
+      p "puts 4 numbers, u have #{5 - @error} trys"
+      @user_input = STDIN.gets.chomp.to_i
+      win
+      get_bulls
+      get_cow
+      print_status
+      @error += 1
+    end
+    puts "u loose"
+  end
+
+  def win
+    if @uniq_random_number.join.to_i == @user_input
+      puts "bingo, u win!"
+      exit
+    end
+
+  end
+
+  def get_bulls
+    @array_bulls = []
+    @user_input = @user_input.to_s.split('').map(&:to_i)
+    @user_input.size.times do |i|
+      @array_bulls << @user_input[i] if
+      @uniq_random_number[i] == @user_input[i]
+    end
+  end
+
+  def get_cow
+    @array_cow = []
+    @user_input -= @array_bulls
+    @array_cow << (@uniq_random_number & @user_input)
+  end
+
+  def print_status
+    puts "#{@array_bulls.uniq.join(',')} buls" if @array_bulls != []
+    puts "#{@array_cow.uniq.join(',')} cow" if @array_cow != []
+  end
+
+# def errors
+#   error < turn_numbers?
+# end
+
 end
 
-puts "u loose"
+b = Bulls.new
+b.get_unirandnumber
+b.run
